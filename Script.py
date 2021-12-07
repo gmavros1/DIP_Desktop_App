a9172471eb5916386e5bce57b3f2f101ebd8519d
gmavros1 <gmavrosi@csd.auth.gr>
2021-12-08 01:54:05 +0200

    it works

--- Script.py
+++ Script.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 import cv2
 
 
@@ -24,7 +25,7 @@ def myConv2(A, B, param):
     paddingWidth = (kernelWidth // 2)
 
     # padding for convolution
-    paddedImage = np.zeros((imageHeight + 2 * paddingHeight, imageWidth + 2 * paddingWidth), dtype=np.int8)
+    paddedImage = np.zeros((imageHeight + 2 * paddingHeight, imageWidth + 2 * paddingWidth), dtype=int)
     im = 0
     for i in range(paddingHeight, imageHeight + 1):
         jm = 0
@@ -38,51 +39,58 @@ def myConv2(A, B, param):
     outputWidth = sizeAfterConv(imageWidth, kernelWidth, paddingWidth, paddingWidth, 1)
 
     # flip kernel
-    for i in range(paddingHeight):
-        for j in range(paddingWidth + 1):
-            B[i][j], B[paddingHeight - i + 1][paddingWidth - j + 1] = B[paddingHeight - i + 1][paddingWidth - j + 1], B[i][j]
+    B = np.fliplr(B)
+    B = np.flipud(B)
 
-    out = np.zeros((outputHeight, outputWidth), dtype=np.int8)
+    out = np.zeros(outputHeight * outputWidth, dtype=int)
 
-    for k in range(outputHeight):
-        for l in range(outputWidth):
-            for i in range(len(paddedImage) - kernelHeight):
-                for j in range(len(paddedImage[0]) - kernelWidth):
-                    convSum = 0
-                    for m in range(kernelHeight):
-                        for n in range(kernelWidth):
-                            convSum += paddedImage[n + i][n + j] * B[m][n]
-                    out[k][l] = convSum
+    count = 0
+    for i in range(len(paddedImage) - kernelHeight + 1):
+        for j in range(len(paddedImage[0]) - kernelWidth + 1):
+            convSum = 0
+            for m in range(kernelHeight):
+                for n in range(kernelWidth):
+                    convSum += paddedImage[m + i][n + j] * B[m][n]
+            out[count] = convSum
+            count += 1
+
+    out2D = np.reshape(out, (outputHeight, -1))
 
     if param == 'same':
         for i in range(imageHeight):
             for j in range(imageWidth):
-                A[i][j] = out[i + (outputHeight - imageHeight)][j + outputWidth - imageWidth]
+                A[i][j] = out2D[i + (outputHeight - imageHeight)][j + outputWidth - imageWidth]
         return np.array(A)
     else:
-        return out
+        return out2D
 
 
-matrix = [[3, 23, 255, 6, 23, 87, 33, 54, 1, 8],
-          [32, 67, 255, 65, 7, 81, 87, 52, 79, 23],
-          [1, 23, 255, 6, 22, 87, 200, 54, 2, 8],
-          [31, 23, 255, 255, 23, 87, 33, 54, 1, 8],
-          [100, 23, 255, 6, 23, 87, 33, 54, 1, 8],
-          [150, 12, 57, 112, 34, 25, 56, 12, 23, 56],
-          [5, 4, 43, 255, 255, 255, 255, 255, 6, 2],
-          [3, 23, 6, 6, 23, 87, 33, 54, 1, 8]]
+matrix = np.array([[3, 23, 255, 6, 23, 87, 33, 54, 1, 8],
+                   [32, 67, 255, 65, 7, 81, 87, 52, 79, 23],
+                   [1, 23, 255, 6, 22, 87, 200, 54, 2, 8],
+                   [31, 23, 255, 255, 23, 87, 33, 54, 1, 8],
+                   [100, 23, 255, 6, 23, 87, 33, 54, 1, 8],
+                   [150, 12, 57, 112, 34, 25, 56, 12, 23, 56],
+                   [5, 4, 43, 255, 255, 255, 255, 255, 6, 2],
+                   [3, 23, 6, 6, 23, 87, 33, 54, 1, 8]])
 
-kernel = [[-1, 0, 1],
-          [-1, 0, 1],
-          [-1, 0, 1]]
-
-out = myConv2(matrix, kernel, 'pad')
-
-plt.subplot(3, 1, 1)
+plt.subplot(2, 2, 1)
 plt.imshow(matrix, cmap='gray')
-plt.subplot(3, 1, 2)
-plt.imshow(kernel, cmap='gray')
-plt.subplot(3, 1, 3)
-plt.imshow(out, cmap='gray')
-plt.show()
 
+kernel = np.array([[-1, 0, 1],
+                   [-1, 0, 1],
+                   [-1, 0, 1]])
+
+
+plt.subplot(2, 2, 2)
+plt.imshow(kernel, cmap='gray')
+
+out1 = signal.convolve2d(matrix, kernel, boundary='symm', mode='same')
+plt.subplot(2, 2, 4)
+plt.imshow(out1, cmap='gray')
+
+out = myConv2(matrix, kernel, 'same')
+plt.subplot(2, 2, 3)
+plt.imshow(out, cmap='gray')
+
+plt.show()
